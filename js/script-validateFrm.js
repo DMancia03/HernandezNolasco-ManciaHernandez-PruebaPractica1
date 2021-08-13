ea10bc9bca7ae6a744e8527e7cc2f54b0c0d96c8
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2021-08-13 10:45:09 -0600

    ultimo

--- js/script-validateFrm.js
+++ js/script-validateFrm.js
@@ -12,8 +12,8 @@ window.onload = function(){
         var sltPResidencia = document.getElementById("pResidencia");
         var sltDResidencia = document.getElementById("dResidencia");
         var txtDomicilio = document.getElementById("txtDomicilio");
-        var txtTelefono1 = document.getElementById("txtTelefono");
-        var  txtTelefono2 = document.getElementById("txtTelefono");
+        var txtTelefono = document.getElementById("txtTelefono");
+        var  txtCelular = document.getElementById("txtCelular");
 
         if(txtApellidos.value == ""){
             errores++;
@@ -42,11 +42,17 @@ window.onload = function(){
         if(txtDomicilio.value == ""){
             errores++;
         }
+        if(txtTelefono.value == ""){
+            errores++;
+        }
+        if(txtCelular.value == ""){
+            errores++;
+        }
 
         if(errores > 0){
             alert("Llene correctamente los campos.");
         }else{
-            var candidato = Persona(txtNombres.value, txtApellidos.value, dtNacimiento.value, rdbGenero.value, stcPaisNacimiento , tel, email, phone, nowCountry, dep, direction );
+            var candidato = new Persona(txtNombres.value, txtApellidos.value, dtNacimiento.value, rdbGenero.value, stcPaisNacimiento.value , txtTelefono.value, emailCandidato.value, txtCelular.value, sltPResidencia.value, sltPResidencia.value, txtDomicilio.value );
         }
     }
 }
